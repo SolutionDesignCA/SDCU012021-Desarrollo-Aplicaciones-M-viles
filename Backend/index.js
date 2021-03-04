99169debf6b0ab486e44a0547eaa9e68713c4b17
juanferdeleon <dele17822@uvg.edu.gt>
2021-03-04 11:06:44 -0600

    gitignore

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 const callback = () => {
   console.log(`El servidor esta inicializado en el puerto ${process.env.PORT}`);
 };
 
 app.listen(process.env.PORT, callback);
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo!");
+});
